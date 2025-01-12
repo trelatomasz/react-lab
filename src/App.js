c0b93c3dff38d521fc86120ab1c5fb6af833b9be
tomastrela <pikson.tom@gmail.com>
2025-01-12 10:53:42 +0100

    [react] added reactivness

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let title = 'Artificial Intelligence: AI'
-     function handleChange(event) {
-        console.log(event.target.value);
+    // Whole component is rendered
+    console.log("Component App is not regenerated");
+
+    let [title, setTitle] = useState('Artificial Intelligence: AI');
+    let errorMsg = '';
+
+    if (title.length < 3) {
+        errorMsg = 'Title to short. Minimum length: 3';
+    } else if (title.length > 20) {
+        errorMsg = 'Title too long. Maximum length: 20';
     }
 
+    function handleChange(event) {
+        console.log("Input changed");
+
+        setTitle(event.target.value);
+    };
     return (
-      <div>
-          <h1>My favourite movies to watch</h1>
-          <h2>My favourite movie for today: {title}</h2>
-          <input type="text" onChange={handleChange}/>
-      </div>
-  );
+        <div>
+            <h1>My favourite movies to watch</h1>
+            <h2>My favourite movie for today:</h2>
+            <h3>{title}</h3>
+            <input type="text" value={title} onChange={handleChange}/>
+            {title.length > 0 && <div>{errorMsg}</div>}
+
+        </div>
+    );
 }
 
 export default App;
